1b05afa5096b82d6688d53c0acd08466a7a566ed
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-10-24 16:50:06 +0300

    fix: fix eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { Root } from './Root';
 
-ReactDOM.render(
-  <Root />,
-  document.getElementById('root'),
+// ReactDOM.render(
+//   <Root />,
+//   document.getElementById('root'),
+// );
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
 );
