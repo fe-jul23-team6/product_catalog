90a4193067ce1d7f6ceacc145a9edab24840fc33
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-10-24 18:01:43 +0300

    fix:ESLINT FINAL FIX

--- src/Root.tsx
+++ src/Root.tsx
@@ -13,14 +13,14 @@ import { NotFoundPage } from './pages/NotFoundPage';
 export const Root = () => (
   <Router>
     <Routes>
-      <Route path='/' element={<App />}>
+      <Route path="/" element={<App />}>
         <Route index element={<HomePage />} />
         <Route path="phones">
           <Route index element={<CatalogPage />} />
           <Route path=":PhoneId" element={<ItemCardPage />} />
         </Route>
         <Route path="cart" element={<CartPage />} />
-        <Route path='*' element={<NotFoundPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   </Router>
