03cadd6b48d1b402ea7a3ccad1766924b8816be5
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-10-25 15:13:29 +0300

    fix: change files to resolve conflicts between devs

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
-import { Footer } from './components/Footer';
+
 import './index.scss';
 
 export const App = () => {
   return (
-    <html lang="en" className="page">
-      <body className="page__body">
-        <Header />
-        <main className="section">
-          <div className="container">
-            <Outlet />
-          </div>
-        </main>
-        <Footer />
-      </body>
-    </html>
+    <div className="page">
+      <Header />
+      <main className="page__body">
+
+        <Outlet />
+      </main>
+    </div>
   );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,59 @@
-import { Topbar } from '../Topbar';
+import { NavLink } from 'react-router-dom';
+import logo from 'assets/img/logo.svg';
+import { ReactComponent as Heart }
+  from 'assets/img/icons/favourites-default_icon.svg';
+import { ReactComponent as Cart }
+  from 'assets/img/icons/shopping-bag_icon.svg';
 
 export const Header = () => {
   return (
-    <>
-      <header className="header" id="header">
-        <div className="container">
-          <div className="header__content">
-            <Topbar />
-            <div className="header__bottom">
-              <h1
-                className="header__title"
-              >
-                Welcome to Nice Gadgets store!
-              </h1>
-            </div>
-          </div>
-        </div>
-      </header>
-    </>
+    <header className="header">
+      <div className="header__left">
+        <a
+          href="http://localhost:3000/product_catalog#/"
+          className="header__logo"
+        >
+          <img
+            className="header__logo-size"
+            src={logo}
+            alt="Nice Gadgets logo"
+          />
+        </a>
+
+        <nav className="header__menu">
+          <NavLink to="/">
+            Home
+          </NavLink>
+
+          <NavLink to="/phones">
+            Phones
+          </NavLink>
+
+          <NavLink to="/tablets">
+            Tablets
+          </NavLink>
+
+          <NavLink to="/accessories">
+            Accessories
+          </NavLink>
+        </nav>
+      </div>
+
+      <div className="header__icons">
+        <NavLink
+          to="/cart"
+          className="header__icon"
+        >
+          <Heart />
+        </NavLink>
+
+        <NavLink
+          to="/cart"
+          className="header__icon"
+        >
+          <Cart />
+        </NavLink>
+      </div>
+    </header>
   );
 };
