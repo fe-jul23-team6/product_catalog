6b92c3d651e82851eb30f2e1ee182a5441a64961
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-10-27 18:02:06 +0300

    feat: cart item

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,59 +1,83 @@
 import { ReactComponent as CloseSvg } from 'assets/img/icons/close_icon.svg';
-import './CartItem.scss';
 import { ReactComponent as PlusSvg } from 'assets/img/icons/plus_icon.svg';
 import { ReactComponent as MinusSvg } from 'assets/img/icons/minus_icon.svg';
 import { useState } from 'react';
 import { BASE_URL } from 'utils/fetchProducts';
+import { Phone } from 'types';
+import styles from './CartItem.module.scss';
 
-export const CartItem = () => {
-  const [count, setCount] = useState(1);
+type Props = {
+  phone: Phone,
+};
+
+export const CartItem: React.FC<Props> = ({ phone }) => {
+  const [count, setCount] = useState(0);
+
+  const substract = (event: React.MouseEvent) => {
+    event.preventDefault();
+    if (count === 0) {
+      return;
+    }
+
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  const add = (event: React.MouseEvent) => {
+    event.preventDefault();
+    if (count === 99) {
+      return;
+    }
+
+    setCount((prevCount) => prevCount + 1);
+  };
 
   return (
-    <div className="cart-item">
-      <div className="cart-item__top">
-        <CloseSvg className="cart-item__remove" />
-        <div className="cart-item__top-container">
+    <div className={styles['cart-item']}>
+      <div className={styles['cart-item__top']}>
+        <CloseSvg className={styles['cart-item__remove']} />
+        <div className={styles['cart-item__top-container']}>
           <img
-            className="cart-item__img"
-            src={`${BASE_URL}/img/phones/apple-iphone-11-pro/gold/01.jpg`}
-            alt="product"
+            className={styles['cart-item__img']}
+            src={`${BASE_URL}/${phone.image}`}
+            alt={phone.name}
           />
         </div>
-        <h4 className="cart-item__title">
-          Apple iPhone 11 Pro Max 64GB Gold (iMT9GFS/A)
+        <h4 className={styles['cart-item__title']}>
+          {phone.name}
         </h4>
       </div>
 
-      <div className="cart-item__bottom">
-        <div className="cart-item__bottom--left">
+      <div className={styles['cart-item__bottom']}>
+        <div className={styles['cart-item__bottom--amount']}>
           <button
             type="button"
-            className="cart-item__button"
+            className={`${styles['cart-item__button--disabled']} ${styles['cart-item__button']}`}
             onClick={(event) => {
-              event.preventDefault();
-              setCount((prevCount) => prevCount - 1);
+              substract(event);
             }}
           >
             <MinusSvg />
           </button>
 
-          <span>
+          <span className={styles['cart-item__count']}>
             {count}
           </span>
 
           <button
             type="button"
-            className="cart-item__button"
+            className={styles['cart-item__button']}
             onClick={(event) => {
-              event.preventDefault();
-              setCount((prevCount) => prevCount + 1);
+              add(event);
             }}
           >
             <PlusSvg />
           </button>
         </div>
 
-        <p className="cart-item__price">$1099</p>
+        <p className={styles['cart-item__price']}>
+          $
+          {phone.price}
+        </p>
       </div>
     </div>
   );
