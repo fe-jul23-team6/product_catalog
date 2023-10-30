ae75fd70336e9725a4ac9d4a00ddb77dc4d73e20
Oleksii Andriushyn <andriushyn.aleks@gmail.com>
2023-10-30 23:00:12 +0200

    feat: add big slider

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,19 @@
 import { useState, useEffect } from 'react';
-import { PageTitle } from 'components/PageTitle';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
 import { getPhones } from 'services/products.service';
+import { PageTitle } from 'components/PageTitle';
 import { SliderSmall } from 'components/SliderSmall';
+import { Button } from 'components/UI/Buttons';
+import iPhone14Pro from 'assets/img/banner-dark.png';
+import iPhones from 'assets/img/banner-phones-dark.png';
+import iTabs from 'assets/img/banner-tablets-dark.jpg';
+import Accessories from 'assets/img/banner-accessories-dark.png';
 import { Phone } from 'types';
 import styles from './HomePage.module.scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss';
 
 export const HomePage = () => {
   const [newModels, setNewModels] = useState<Phone[]>([]);
@@ -44,9 +54,63 @@ export const HomePage = () => {
         title="Welcome to Nice Gadgets store!"
       />
 
-      <div className={styles['home__slider-available']}>
-        {}
-      </div>
+      <section className={styles.home__sliderBig}>
+        <div className="button-prev">
+          <Button
+            btnType="Slider"
+            chevronButtonType="left"
+            shevron
+          />
+        </div>
+
+        <Swiper
+          loop
+          modules={[Navigation, Pagination]}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={{
+            prevEl: '.button-prev',
+            nextEl: '.button-next',
+          }}
+          slidesPerView={1}
+        >
+          <SwiperSlide>
+            <img
+              src={iPhone14Pro}
+              alt="iPhone 14 Pro"
+              className={styles['header__logo-size']}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={iPhones}
+              alt="iPhones"
+              className={styles['header__logo-size']}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={iTabs}
+              alt="iTabs"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={Accessories}
+              alt="Apple Accessories"
+            />
+          </SwiperSlide>
+        </Swiper>
+
+        <div className="button-next">
+          <Button
+            btnType="Slider"
+            chevronButtonType="right"
+            shevron
+          />
+        </div>
+      </section>
 
       <SliderSmall
         selectedPhones={newModels}
