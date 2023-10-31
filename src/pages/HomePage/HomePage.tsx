8cf48372b7f0063c1df779e5b90aec44268eaa12
Oleksii Andriushyn <andriushyn.aleks@gmail.com>
2023-10-31 16:38:40 +0200

    fix: doublelink deleted

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import { PageTitle } from 'components/PageTitle';
