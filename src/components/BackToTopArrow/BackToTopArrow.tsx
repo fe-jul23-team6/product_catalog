6cf67a0cfa4b2bea6f594cf489da2625337606d6
Oleksii Andriushyn <andriushyn.aleks@gmail.com>
2023-11-01 22:53:52 +0200

    fix: add BackToTopArrow for smart scrolling + fix h2 titles size on HomePage

---
+++ src/components/BackToTopArrow/BackToTopArrow.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { ReactComponent as ArrowSvg } from 'assets/img/icons/chevron-up_icon.svg';
+import styles from './BackToTopArrow.module.scss';
+
+export const BackToTopArrow = () => {
+  const [scrolling, setScrolling] = useState(false);
+
+  const handleScroll = () => {
+    const windowHeight = window.innerHeight;
+
+    const documentHeight = document.documentElement.scrollHeight;
+
+    const scrollPosition = (documentHeight - windowHeight - window.scrollY) >= 100;
+
+    if (window.scrollY > 100 && scrollPosition) {
+      setScrolling(true);
+    } else {
+      setScrolling(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className={`${styles.backToTopArrow} ${styles[`${scrolling ? 'scrolling' : ''}`]}`}>
+      <NavLink
+        to="#"
+        onClick={scrollToTop}
+      >
+        <div className={styles.backToTopArrow__block}>
+          <ArrowSvg className={styles.backToTopArrow__img} />
+        </div>
+      </NavLink>
+    </div>
+  );
+};
