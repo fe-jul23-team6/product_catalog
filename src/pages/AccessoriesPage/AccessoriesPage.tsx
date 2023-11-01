c593a986d56d39ef2225e1f953386b61bd05336f
Yevheniia Sidanchenko <sidanchenkoe@gmail.com>
2023-11-01 15:48:39 +0200

    feat: add tablets and accessories pages

--- src/pages/AccessoriesPage/AccessoriesPage.tsx
+++ src/pages/AccessoriesPage/AccessoriesPage.tsx
@@ -1,6 +1,111 @@
-import { PageTitle } from 'components/PageTitle';
-// import styles from './AccessoriesPage.module.scss';
+import React, { useEffect, useState, useCallback } from 'react';
+import { Outlet, useSearchParams } from 'react-router-dom';
 
-export const AccessoriesPage = () => (
-  <PageTitle title="Accessories" />
-);
+import { Loader } from 'components/UI/Loader';
+import { CatalogTable } from 'components/CatalogTable';
+import { PageTitle } from 'components/PageTitle';
+import { Dropdown } from 'components/UI/Dropdown';
+import { Pagination } from 'components/Pagination';
+import { PageLocation } from 'components/UI/PageLocation';
+import {
+  DEFAULT_PAGE,
+  DEFAULT_SORT_BY,
+  MESSAGES,
+  PAGE_SIZE_OPTIONS,
+  SORT_OPTION,
+} from 'utils/constants';
+import { SortOption } from 'types/SortOption';
+import { getAccessories } from 'services/products.service';
+import { Phone } from 'types';
+import styles from './AccessoriesPage.module.scss';
+
+export const AccessoriesPage = () => {
+  const [accessories, setAccessories] = useState<Phone[]>([]);
+  const [itemsCount, setItemsCount] = useState(0);
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+
+  const [searchParams] = useSearchParams();
+  const page = searchParams.get('page') || DEFAULT_PAGE.toString();
+  const perPage = searchParams.get('perPage') || null;
+  const sort = (searchParams.get('sort') || DEFAULT_SORT_BY) as keyof typeof SortOption;
+
+  const getItems = useCallback(() => {
+    setIsLoading(true);
+
+    getAccessories(sort, page, perPage)
+      .then((dataFromServer) => {
+        setIsLoading(false);
+        setAccessories(dataFromServer.rows);
+        setItemsCount(dataFromServer.count);
+      })
+      .catch(() => {
+        setHasError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [page, perPage, sort]);
+
+  useEffect(() => {
+    getItems();
+  }, [page, perPage, sort]);
+
+  const hasErrorMessage = hasError && !isLoading;
+  const hasNoItemsOnServer = !itemsCount && !hasError && !isLoading;
+
+  return (
+    <section className={styles.catalog}>
+      <PageLocation to="/accessories" text="Accessories" />
+
+      <PageTitle title="Accessories" />
+
+      {isLoading && (<Loader />)}
+
+      {hasErrorMessage && (
+        <p>{MESSAGES.NO_SERVER_RESPONSE}</p>
+      )}
+
+      {hasNoItemsOnServer && (
+        <p>
+          {MESSAGES.NO_ACCESSORIES}
+        </p>
+      )}
+
+      {!!accessories.length && (
+        <section>
+          <p className={styles['catalog__items-count']}>{`${itemsCount} models`}</p>
+
+          <div className={styles['catalog__dropdown-container']}>
+            <div className={styles.catalog__dropdown}>
+              <Dropdown
+                description="Sort by"
+                options={SORT_OPTION}
+                query={sort}
+              />
+            </div>
+
+            <div className={styles.catalog__dropdown}>
+              <Dropdown
+                description="Items on page"
+                options={PAGE_SIZE_OPTIONS}
+                query={perPage}
+              />
+            </div>
+          </div>
+
+          <CatalogTable phones={accessories} />
+
+          <div className={styles.catalog__pagination}>
+            <Pagination
+              total={itemsCount}
+            />
+          </div>
+        </section>
+      )}
+
+      <Outlet />
+    </section>
+  );
+};
