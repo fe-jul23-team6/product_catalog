2b8dfd3c76cc6a83f6c3f04e0db0af0b9e1c8ea1
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-11-01 14:13:10 +0200

    feat: contacts page

---
+++ src/utils/contactsData.ts
@@ -0,0 +1,53 @@
+import OlyaPhoto from '../assets/img/devs/dev-olya.jpg';
+import JennyPhoto from '../assets/img/devs/dev-jenny.jpg';
+import DianaPhoto from '../assets/img/devs/dev-diana.jpg';
+import OleksiiPhoto from '../assets/img/devs/dev-oleksii.jpg';
+import RomaPhoto from '../assets/img/devs/dev-roma.jpg';
+
+export const contactsData = [
+  {
+    photo: DianaPhoto,
+    name: 'Diana Honcharova',
+    position: 'Full-stack Developer',
+    linkedIn: 'https://www.linkedin.com/in/diana-honcharova-08a2131a7',
+    gitHub: 'https://github.com/polinavafik',
+    telegram: 'https://t.me/polinavafik',
+    email: 'diana.honcharova.dev@gmail.com',
+  },
+  {
+    photo: JennyPhoto,
+    name: 'Yevheniia Sidanchenko',
+    position: 'Full-stack Developer',
+    linkedIn: 'https://www.linkedin.com/in/yevheniia-sidanchenko-479061106/',
+    gitHub: 'https://github.com/Yevheniia-Sid',
+    telegram: 'https://t.me/Sidanchenko_E',
+    email: 'yevheniia.sidanchenko@gmail.com',
+  },
+  {
+    photo: OlyaPhoto,
+    name: 'Olga Katiushyna',
+    position: 'Full-stack Developer',
+    linkedIn: 'https://www.linkedin.com/in/ola-katiushyna-katolista/',
+    gitHub: 'https://github.com/KatOlista',
+    telegram: 'https://t.me/KatOlista',
+    email: 'ola.katiushyna@gmail.com',
+  },
+  {
+    photo: OleksiiPhoto,
+    name: 'Oleksii Andriushyn',
+    position: 'Full-stack Developer',
+    linkedIn: 'https://www.linkedin.com/in/oleksii-andriushyn-859907192/',
+    gitHub: 'https://github.com/Orpheus29',
+    telegram: 'https://t.me/orpheus29',
+    email: 'oleksii.andriushyn.dev@gmail.com',
+  },
+  {
+    photo: RomaPhoto,
+    name: 'Roman Yatsykovych',
+    position: 'Full-stack Developer',
+    linkedIn: 'https://www.linkedin.com/in/roman-yatsykovych-09b712291/',
+    gitHub: 'https://github.com/yar0man',
+    telegram: 'https://t.me/roman_yatsykovych',
+    email: 'roman.yatsykovych@gmail.com',
+  },
+];
